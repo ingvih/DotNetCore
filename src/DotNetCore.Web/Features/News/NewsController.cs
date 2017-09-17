b4897a31db6fb503a0f77685108dc3757a513d72
Ingvi Rafn Hafþórsson <ingvih@gmail.com>
2017-09-17 18:01:33 +0000

    Added NewsController to be used as an example.

---
+++ src/DotNetCore.Web/Features/News/NewsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCore.Web.Features.News
+{
+    public class NewsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
