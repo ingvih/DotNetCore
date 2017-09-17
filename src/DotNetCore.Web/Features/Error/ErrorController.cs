418f0a0858754ae46a38fe948cb4bb9a91799e52
Ingvi Rafn Hafþórsson <ingvih@gmail.com>
2017-09-17 19:49:42 +0000

    Created Error Feature.

---
+++ src/DotNetCore.Web/Features/Error/ErrorController.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using DotNetCore.Web.Features.Home;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCore.Web.Features.Error
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- src/DotNetCore.Web/Features/Home/HomeController.cs
+++ src/DotNetCore.Web/Features/Home/HomeController.cs
@@ -12,10 +12,5 @@ namespace DotNetCore.Web.Features.Home
             string value = helperClass.GetTestValue("Value from Web.");
             return View();
         }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
